c1052310ad332f5943b747396c87f63f81712f4b
MuHuawen <1535309390@qq.com>
2022-12-23 15:00:14 +0800

    master commit2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,5 +10,6 @@ public class Main {
         System.out.println("Hello world3!");
         System.out.println("Hello world3!");
         System.out.println("Hello world4!");
+        System.out.println("master test!");
     }
 }
